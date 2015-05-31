0a40ec7d3c944b9023251bb8051bf8bb3073a48a
fabianterhorst <fabian.terhorst@gmail.com>
2015-05-31 15:45:28 +0200

    offline mode

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,3 @@
-/*
-Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
-*/
-
 (function (document) {
   'use strict';
 
@@ -24,6 +15,13 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   app.addEventListener('template-bound', function() {
     console.log('Our app is ready to rock!');
   });
+  
+  
+      window.addEventListener('WebComponentsReady', function() {
+        // Explicitly call the register() method. We need to wait until the template's variables are
+        // all set first, since the configuration depends on bound variables.
+        document.querySelector('platinum-sw-register').register();
+      });
 
   // See https://github.com/Polymer/polymer/issues/1381
   window.addEventListener('WebComponentsReady', function() {
